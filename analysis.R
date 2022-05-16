1c0f683f3e9dad31286d4d92ff679dd3fac42939
mj-aims <m.jonker@aims.gov.au>
2022-05-16 13:29:32 +1000

    summary, mean and sd of y with barplot

--- analysis.R
+++ analysis.R
@@ -1,6 +1,12 @@
 x <- seq(1,10,len = 1) #an assignment operator - assigning a value of something to a variable
 y = 40*2 +rnorm (10,0,5)
 plot(x,y)
-summary(x)
+summary(x) # all values are 1. 
 mean(x)
 sd(x)
+
+summary (y)
+mean (y)
+sd(y)
+barplot (y,x)
+
